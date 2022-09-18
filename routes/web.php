400082a02f4eb5f7c125186c63869d99cde4185d
Hidran Arias <hidran.arias@spryker.com>
2022-09-18 18:17:39 +0200

    53. uses User model to seed users table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/users', function () {
+    return  User::paginate(5);
+});
 
 Route::get('/', function () {
     return view('welcome');
