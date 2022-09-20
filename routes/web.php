3f5001efe5004cec62c15f9c1b3f45cbc0e88dab
Hidran Arias <hidran@gmail.com>
2022-09-20 22:07:02 +0200

    74. Laravel query builder joins. Trovare utenti senza album

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,16 @@ Route::get('/users', function () {
 Route::resource('/albums', AlbumsController::class);
 Route::delete('/albums/{album}/delete', [AlbumsController::class, 'delete']);
 Route::get('/', [AlbumsController::class, 'index']);
+
+Route::get('usersnoalbums', function () {
+    $usersnoalbum = DB::table('users  as u')
+        ->leftJoin('albums as a', 'u.id', 'a.user_id')
+        ->select('u.id', 'email', 'name', 'album_name')->
+        whereRaw('album_name is null')
+        ->get();
+    $usersnoalbum = DB::table('users  as u')
+        ->select('u.id', 'email', 'name')->
+        whereRaw(' EXISTS (SELECT user_id from albums where user_id= u.id)')
+        ->get();
+    return $usersnoalbum;
+});
