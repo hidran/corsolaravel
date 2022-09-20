8f43b863eee548b7785ee7708aa8d002ed87f993
Hidran Arias <hidran.arias@spryker.com>
2022-09-20 10:54:51 +0200

    63. adds delete method and csrf protection: _token

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,12 @@ use App\Http\Controllers\AlbumsController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/users', function () {
     return User::with('albums')->paginate(5);
 });
 //Route::get('/albums', [AlbumsController::class, 'index']);
 Route::resource('/albums', AlbumsController::class);
-Route::get('/albums/{album}/delete', [AlbumsController::class, 'delete']);
+Route::delete('/albums/{album}/delete', [AlbumsController::class, 'delete']);
 Route::get('/', function () {
     return view('welcome');
 });
