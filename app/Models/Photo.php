c0a99479d4ce532fdce74027f90a3e5012c9757a
Hidran Arias <hidran@gmail.com>
2022-09-27 19:06:09 +0200

    96. Updates image data

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -38,4 +38,18 @@ class Photo extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function album()
+    {
+        return $this->belongsTo(Album::class);
+    }
+
+    public function getPathAttribute()
+    {
+        $url = $this->img_path;
+        if (!str_starts_with($url, 'http')) {
+            $url = 'storage/' . $url;
+        }
+        return $url;
+    }
 }
