0be28f4df5743af612e1dc47e9cb91aef533b5d9
Hidran Arias <hidran@gmail.com>
2023-02-19 18:53:14 +0100

    Frontend Gallery. Mostrare immagini con il plugin lightbox

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -10,6 +10,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/lightbox.css">
     <style>
         body {
             padding: 70px 15px 0;
@@ -36,7 +37,7 @@
                 @auth
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/albums">Home</a>
+                            <a class="nav-link active" aria-current="page" href="{{route('gallery.index')}}">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('albums.index')}}">Albums</a>
@@ -97,6 +98,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
             crossorigin="anonymous"></script>
+    <script src="/js/lightbox.js"></script>
 @show
 </body>
 </html>
